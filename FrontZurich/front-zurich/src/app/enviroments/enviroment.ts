3cfb334625c8d2ebb111aad1d800443ad5cd98cf
MontieMJ <jmmontielmendoza@gmail.com>
2025-10-31 12:06:47 -0600

    url

--- FrontZurich/front-zurich/src/app/enviroments/enviroment.ts
+++ FrontZurich/front-zurich/src/app/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7128/api',
+  apiUrl: 'http://localhost:8080/api',
   apiKey: 'default-dev-key',
   version: '1.0.0'
 };
